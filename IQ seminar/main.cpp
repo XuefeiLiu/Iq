e86de095dba485ccdd6803e76d837c8aa5354aa4
XuefeiLiu <liuxuefei01@gmail.com>
2016-08-27 11:18:45 -0400

    Initial Commit

---
+++ IQ seminar/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  IQ seminar
+//
+//  Created by xuefei liu on 8/27/16.
+//  Copyright © 2016 xuefei liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
